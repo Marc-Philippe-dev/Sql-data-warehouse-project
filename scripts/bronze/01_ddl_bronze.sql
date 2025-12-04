56b6d1155662c41ae0ad01ad023b60cf4599354a
GNANCADJA Marc Philippe <marcgnancadja4@gmail.com>
2025-12-04 21:37:33 +0100

    feat : create ddl statements for the bronze layer

---
+++ scripts/bronze/01_ddl_bronze.sql
@@ -0,0 +1,110 @@
+
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+BEGIN
+	DROP TABLE bronze.crm_cust_info;
+	PRINT 'Existing table "bronze.crm_cust_info" dropped.';
+END;
+
+CREATE TABLE bronze.crm_cust_info(
+	cst_id INT , 
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(10),
+	cst_gender NVARCHAR(10),
+	cst_created_date DATE 
+);
+GO
+PRINT '✓ Table "bronze.crm_cust_info" created.';
+
+
+
+IF OBJECT_ID('bronze.crm_prd_info' , 'U') IS NOT NULL 
+BEGIN
+	DROP TABLE bronze.crm_prd_info ;
+	PRINT 'Existing table "bronze.crm_prd_info" dropped.';
+END;
+
+CREATE TABLE bronze.crm_prd_info(
+	prd_id INT , 
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(100),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
+);
+GO
+PRINT '✓ Table "bronze.crm_prd_info" created.';
+
+
+
+
+IF OBJECT_ID('bronze.crm_sales_details' , 'U') IS NOT NULL 
+BEGIN
+	DROP TABLE bronze.crm_sales_details ;
+	PRINT 'Existing table "bronze.crm_sales_details" dropped.';
+END;
+
+CREATE TABLE bronze.crm_sales_details(
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT ,
+	sls_order_dt DATETIME,
+	sls_ship_dt DATETIME,
+	sls_due_dt DATETIME,
+	sls_sales INT,
+	sls_quantity INT ,
+	sls_price INT	
+)
+GO
+
+PRINT '✓ Table "bronze.crm_sales_details" created.'; 
+
+
+
+IF OBJECT_ID('bronze.erp_cust_az12 ' , 'U') IS NOT NULL
+BEGIN
+	DROP TABLE bronze.erp_cust_az12 ;
+	PRINT 'Existing table "bronze.erp_cust_az12" dropped.';
+END;
+
+
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+
+GO
+PRINT '✓ Table "bronze.erp_cust_az12" created.';
+
+
+IF OBJECT_ID('bronze.erp_loc_a101' , 'U') IS NOT NULL
+BEGIN
+	DROP TABLE bronze.erp_loc_a101 ;
+	PRINT 'Existing table "bronze.erp_loc_a101" dropped.';
+END;	
+
+
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+GO
+
+PRINT '✓ Table "bronze.erp_loc_a101" created.';
+
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+GO
+PRINT '✓ Table "bronze.erp_px_cat_g1v2" created.';
